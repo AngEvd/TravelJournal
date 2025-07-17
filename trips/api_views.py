eb08268cee92d29540b8425a9de84504f9eeebd4
AngEvd <an_evdokimov@yahoo.com>
2025-07-17 15:55:20 +0300

    Public trips search bar added

--- trips/api_views.py
+++ trips/api_views.py
@@ -1,4 +1,5 @@
 import random
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
@@ -16,4 +17,16 @@ class FeaturedTripsAPIView(APIView):
 
 class PublicTripsAPIView(generics.ListAPIView):
     serializer_class = TripSerializer
-    queryset = Trip.objects.filter(is_public=True).order_by('-created_at')
+    
+    def get_queryset(self):
+        queryset = Trip.objects.filter(is_public=True)
+        search_term = self.request.query_params.get('search', None)
+
+        if search_term:
+            queryset = queryset.filter(
+                Q(title__icontains=search_term) |
+                Q(destination__icontains=search_term) |
+                Q(description__icontains=search_term)
+            )
+
+        return queryset.order_by('-created_at')
